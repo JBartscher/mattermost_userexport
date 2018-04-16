da4eb9f333bd197e2bf5b0439eb76dcf5de467da
Jasper <jasper.bartscher@gmail.com>
2018-04-17 01:45:57 +0200

    README and Comments

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mattermost_userexport
-exports all users into a JSON File
+
+exports all users into a JSON File whic will be placed in the same Directory as the skript
+
+open with cmd or shell or anything that can open run a python skript. Should work with python 2 and 3. The current API used is the Mattermost v4 API.
+The skript requieres 3 parameters in the following order: 
+1. You´re Mattermost-Server-URL (e.g: 'https://chat.mattermost.com)
+2. You´re Username or Emailadress
+3. You´re Password
+
+Example:
+>>python3 'https://chat.mattermost.com' 'myuser' 'mypassword'
+
+It will retrieve max. 2000 Users. If you whish to export more (or less) change the PAGES variable. Iam not aware that Mattermost tracks or even blocks Users who try to many connections to the Mattermost API, but iam not intreseted to find it out, so I added a sleep timer (5 Seconds), if you´re in a hurry you can change the wait timer Variable WAIT_SEC to 0.
---
+++ export_mattermost_users.py
@@ -0,0 +1,47 @@
+import requests
+import sys
+from  itertools import repeat
+import time
+import json
+
+PAGES = 10 # will retrive 10*200 Users
+WAIT_SEC = 5
+
+if __name__ == "__main__":
+    #check parametes
+    if len(sys.argv) < 4:
+        sys.exit("There must be 3 Parameters: mattermost server, username or email and password!")
+
+    URL =  sys.argv[1] + '/api/v4' 
+    LOGINURL = URL+"/users/login"
+    USERSURL = URL + "/users"
+
+    #login
+    login_json = {'login_id':sys.argv[2],'password':sys.argv[3]}
+    r = requests.post(LOGINURL, json=login_json)
+    if r.status_code == 200: #login worked
+        print('loged in')
+        auth ={"Authorization" : 'Bearer ' + r.headers['Token']}
+
+        users = {}
+        payload = {
+            'page':0,
+            'per_page': 200
+            }
+ 
+        for i in repeat(None, PAGES):    
+            r = requests.get(USERSURL, params=payload, headers= auth ) #params = querysting
+            if r.status_code == 200: #getting users worked
+                print('retrived data')
+                users['users']= r.json()
+                payload['page'] = payload['page'] +1
+                print("waiting ... 5 seconds")
+                time.sleep(WAIT_SEC) #wait 5 seconds to not look suspices 
+
+        users['date_of_data_retrival'] = time.time()
+
+        # wenn fertig schiebt in .json file
+        with open('user_data.json', 'w') as outfile:
+            json.dump(users, outfile)
+            print("dumped to file user_data.json")
+
